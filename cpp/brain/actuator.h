9125833d6d21281c81087e9222aa248a1d1506f3
Matteo De Carlo <matteo.dek@gmail.com>
2016-04-20 15:12:50 +0200

    Added cpp module sketch

---
+++ cpp/brain/actuator.h
@@ -0,0 +1,17 @@
+#ifndef ACTUATOR_H
+#define ACTUATOR_H
+
+#include <boost/shared_ptr.hpp>
+
+class Actuator
+{
+public:
+    Actuator();
+
+    virtual void update(const double *output_vector, const double step) = 0;
+    virtual unsigned int outputs() = 0;
+};
+
+typedef boost::shared_ptr< Actuator > ActuatorPtr;
+
+#endif // ACTUATOR_H
