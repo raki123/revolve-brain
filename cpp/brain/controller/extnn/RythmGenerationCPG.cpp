31b1838da5819cfab980407f155789e5ee9eccef
Milan Jelisavcic <milan.jelisavcic@gmail.com>
2017-08-02 18:42:03 +0200

    Add `RythmGenerationCPG.*` to project

---
+++ cpp/brain/controller/extnn/RythmGenerationCPG.cpp
@@ -0,0 +1,115 @@
+/*
+ * Copyright (C) 2015-2017 Vrije Universiteit Amsterdam
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Description: TODO: <Add brief description about file purpose>
+ * Author: Milan Jelisavcic
+ * Date: August 2, 2017.
+ *
+ */
+
+#include "RythmGenerationCPG.h"
+
+#include <cmath>
+#include <iostream>
+#include <sstream>
+
+namespace revolve
+{
+  namespace brain
+  {
+
+    RythmGenerationCPG::RythmGenerationCPG(const std::string &id,
+                                           const std::map<std::string, double> &params)
+            : Neuron(id)
+    {
+      if (not params.count("rv:bias"))
+      {
+        std::cerr
+                << "A `"
+                << "Differential CPG"
+                << "` neuron requires `rv:bias` element."
+                << std::endl;
+        throw std::runtime_error("Robot brain error");
+      }
+      this->bias_ = params.find("rv:bias")->second;
+      lastTime_ = 0;
+    }
+
+
+    double RythmGenerationCPG::CalculateOutput(double t)
+    {
+      double deltaT = t - lastTime_;
+      lastTime_ = t;
+
+      if (deltaT > 0.1) deltaT = 0.1;
+
+      double inputValue = 0;
+
+      for (auto it = this->incomingConnections_.begin(); it != this->incomingConnections_.end(); ++it)
+      {
+        auto inConnection = it->second;
+        inputValue += inConnection->GetInputNeuron()->GetOutput() * inConnection->GetWeight();
+      }
+
+      double state_deriv = inputValue - this->bias_;
+      double result = this->output_ + deltaT * state_deriv;
+
+      double maxOut = 10000.0;
+
+      // limit output:
+      // if (result > maxOut) {
+      // 	result = maxOut;
+      // }
+      // else if (result < -maxOut) {
+      // 	result = -maxOut;
+      // }
+
+      // saturate output:
+      double gain = 2.0 / maxOut;
+      result = (2.0 / (1.0 + std::exp(-result * gain)) - 1.0) * maxOut;
+
+      return result;
+    }
+
+    std::map<std::string, double> RythmGenerationCPG::getNeuronParameters()
+    {
+      std::map<std::string, double> ret;
+      ret["rv:bias"] = bias_;
+      return ret;
+    }
+
+    void RythmGenerationCPG::setNeuronParameters(std::map<std::string, double> params)
+    {
+      if (not params.count("rv:bias"))
+      {
+        std::cerr
+                << "A `"
+                << "Differential CPG"
+                << "` neuron requires `rv:bias` element."
+                << std::endl;
+        throw std::runtime_error("Robot brain error");
+      }
+      this->bias_ = params.find("rv:bias")
+                          ->second;
+    }
+
+    std::string RythmGenerationCPG::getType()
+    {
+      return "DifferentialCPG";
+    }
+
+
+  }
+}
---
+++ cpp/brain/controller/extnn/RythmGenerationCPG.h
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2015-2017 Vrije Universiteit Amsterdam
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Description: TODO: <Add brief description about file purpose>
+ * Author: Milan Jelisavcic
+ * Date: August 2, 2017.
+ *
+ */
+
+#ifndef TOL_REVOLVE_RYTHMGENERATIONCPG_H
+#define TOL_REVOLVE_RYTHMGENERATIONCPG_H
+
+#include "ENeuron.h"
+#include "NeuralConnection.h"
+
+namespace revolve
+{
+  namespace brain
+  {
+
+    class RythmGenerationCPG
+            : public Neuron
+    {
+      public:
+      /**
+      * Constructor for differential cpg
+      * @param id: string to identify the neuron
+      * @param params: parameters that specify the behavior of the neuron
+      * @return pointer to the differential cpg
+      */
+      RythmGenerationCPG(const std::string &id,
+                         const std::map<std::string, double> &params);
+
+      /**
+      * Method to calculate the output of the neuron
+      * @param t: current time
+      * @return the output of the neuron at time t
+     	*/
+      virtual double CalculateOutput(double t) override;
+
+      virtual std::map<std::string, double> getNeuronParameters() override;
+
+      virtual void
+      setNeuronParameters(std::map<std::string, double> params) override;
+
+      virtual std::string getType() override;
+
+      protected:
+      double bias_; //bias of the neuron
+      double lastTime_; //last time the output was calculated
+    };
+
+  }
+}
+
+#endif //TOL_REVOLVE_RYTHMGENERATIONCPG_H
